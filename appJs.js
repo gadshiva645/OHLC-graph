d72aeee8c8a9d5c4928506acc681a31a533e6e4b
gadshiva645 <36510481+gadshiva645@users.noreply.github.com>
2019-05-29 19:27:16 -0400

    app files with csv

---
+++ appJs.js
@@ -0,0 +1,105 @@
+
+
+Plotly.d3.csv('datacsv.csv', function(err, rows){
+
+function unpack(rows, key) {
+  return rows.map(function(row) {
+    return row[key];
+  });
+}
+
+var trace = {
+  x: unpack(rows, 'Timestamp'),
+  close: unpack(rows, 'Close'),
+  high: unpack(rows, 'High'),
+  low: unpack(rows, 'Low'),
+  open: unpack(rows, 'Open'),
+
+  // cutomise colors
+  increasing: {line: {color: 'black'}},
+  decreasing: {line: {color: 'red'}},
+
+  type: 'ohlc',
+  xaxis: 'x',
+  yaxis: 'y'
+};
+
+var data = [trace];
+
+var layout = {
+  dragmode: 'pan',
+  showlegend: false,
+  xaxis: {
+    autorange: true,
+    title: 'Date',
+   rangeselector: {
+        x: 0,
+        y: 1.2,
+        xanchor: 'left',
+        font: {size:8},
+        buttons: [{
+            step: 'month',
+            stepmode: 'backward',
+            count: 1,
+            label: '1 month'
+        }, {
+            step: 'month',
+            stepmode: 'backward',
+            count: 6,
+            label: '6 months'
+        }, {
+            step: 'all',
+            label: 'All dates'
+        }]
+      }
+  },
+  yaxis: {
+    autorange: true,
+  }
+};
+
+Plotly.plot('plotly-ohlc-div', data, layout);
+});
+
+
+
+
+
+// Plotly.d3.csv('datacsv.csv', function(err, rows){
+
+// function unpack(rows, key) {
+//   return rows.map(function(row) {
+//     return row[key];
+//   });
+// }
+
+// var trace = {
+//   x: unpack(rows, 'Timestamp'),
+//   close: unpack(rows, 'Close'),
+//   high: unpack(rows, 'High'),
+//   low: unpack(rows, 'Low'),
+//   open: unpack(rows, 'Open'),
+
+//   // cutomise colors
+//   increasing: {line: {color: 'black'}},
+//   decreasing: {line: {color: 'red'}},
+
+//   type: 'ohlc',
+//   xaxis: 'x',
+//   yaxis: 'y'
+// };
+
+// var data = [trace];
+
+// var layout = {
+//   dragmode: 'pan',
+//   showlegend: false,
+//   xaxis: {
+//     rangeslider: {
+//      visible: true;
+//    }
+//   }
+// };
+
+// Plotly.plot('plotly-ohlc-div', data, layout);
+// });
