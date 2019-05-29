0d96e9cd9e00f62d5feac5b6f64a1aebbc2afe4e
gadshiva645 <36510481+gadshiva645@users.noreply.github.com>
2019-05-29 14:58:29 -0400

    using json file without db

--- app.js
+++ app.js
@@ -16,34 +16,11 @@ var express = require("express");
 var app = express();
 var port = 3000;
 
- 
 app.get("/", (req, res) => {
  res.send("Hello World");
 });
  
-
-/*Mongoose and mongodb*/
-var mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/stock-prices", { useNewUrlParser: true });
-
-var nameSchema = new mongoose.Schema({
- timeSeriesDaily: {
- 	stockData: {
- 		date: [Date],
- 		open: [Number],
- 		high: [Number],
- 		low: [Number],
- 		close: [Number],
- 		volume: [Number],
- 	}
- }
-});
-
-var stockUser = mongoose.model("stockUser", nameSchema);
-
 app.use(express.static('public'));
-
 
 
 
@@ -53,31 +30,17 @@ app.listen(port, () => {
 });
 
 
-
-
-
 //pull data from file and save it to json
 const alpha = require('alphavantage')({key: 'JAFMAHL3Q9GK8XC1'});
 
 alpha.data.daily('MSFT', 'compact', 'json', '1min').then(data =>{
-	var obj = {
-		table: []
-	};
-	obj.table.push(data);
-	var json = JSON.stringify(obj);
-	console.log(json);
-
 	var fs = require('fs');
-
-	fs.writeFile ("data.json", JSON.stringify(data), function(err) {
-    if (err) throw err;
-    console.log('complete');
-    }
-);
-
+	fs.writeFile ("datajson.json", JSON.stringify(data, null, '\t'), 
+		function(err) {
+    	if (err) throw err;
+    	console.log('Json file update complete');
+   		}	
+	);
 });
-
-
-
 
 
