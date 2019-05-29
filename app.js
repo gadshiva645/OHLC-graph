738e1730b830bdd5a1387dc307926d1c9d24b0ae
gadshiva645 <36510481+gadshiva645@users.noreply.github.com>
2019-05-29 19:33:43 -0400

    new app file

--- app.js
+++ app.js
@@ -30,17 +30,60 @@ app.listen(port, () => {
 });
 
 
-//pull data from file and save it to json
-const alpha = require('alphavantage')({key: 'JAFMAHL3Q9GK8XC1'});
+// //pull data from file and save it to json
+// const alpha = require('alphavantage')({key: 'JAFMAHL3Q9GK8XC1'});
 
-alpha.data.daily('MSFT', 'compact', 'json', '1min').then(data =>{
-	var fs = require('fs');
-	fs.writeFile ("datajson.json", JSON.stringify(data, null, '\t'), 
-		function(err) {
-    	if (err) throw err;
-    	console.log('Json file update complete');
-   		}	
-	);
+// alpha.data.daily('MSFT', 'compact', 'json', '1min').then(data =>{
+// 	var fs = require('fs');
+// 	fs.writeFile ("datajson.json", JSON.stringify(data, null, '\t'), 
+// 		function(err) {
+//     	if (err) throw err;
+//     	console.log('Json file update complete');
+//    		}	
+// 	);
+// });
+
+
+//////////////////////////////////////////////////////////////////////////
+
+/*csv data*/
+//intialize csvwriter with header format
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;  
+const csvWriter = createCsvWriter({  
+  path: 'datacsv.csv',
+  header: [
+    {id: 'Timestamp', title: 'Timestamp'},
+    {id: 'Open', title: 'Open'},
+    {id: 'High', title: 'High'},
+    {id: 'Low', title: 'Low'},
+    {id: 'Close', title: 'Close'},
+    {id: 'AdjClose', title: 'AdjClose'},
+    {id: 'Volume', title: 'Volume'},
+    {id: 'DividendAmount', title: 'DividendAmount'},
+    {id: 'SplitCoefficient', title: 'SplitCoefficient'},
+  ]
 });
+
+
+//get data from alphavantage and push it to csv file
+var AlphaVantageAPI = require('alpha-vantage-cli').AlphaVantageAPI;
+
+var yourApiKey = 'demo';
+var alphaVantageAPI = new AlphaVantageAPI(yourApiKey, 'compact', true);
+
+alphaVantageAPI.getDailyData('MSFT')
+    .then(dailyData => {
+        console.log("Daily data:");
+        csvWriter  
+  			.writeRecords(dailyData)
+  			.then(()=> console.log('The CSV file was written successfully'));
+        // console.log(dailyData);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+
+
 
 
